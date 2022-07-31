b35f0aeec32f4b4be4579844aa80811291547e35
John Santosuosso <john.santosuosso1@gmail.com>
2022-07-31 16:53:00 -0400

    create facade and add params to it

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,17 @@
 class SearchController < ApplicationController
   def index
-    state = params[:state]
+    @members = SearchFacade.members(params[:state])
+    # state = params[:state]
 
-    conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
-      faraday.headers["X-API-Key"] =  ENV["PROPUBLICA_API_KEY"]
-    end
+    # conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
+    #   faraday.headers["X-API-Key"] =  ENV["PROPUBLICA_API_KEY"]
+    # end
 
-    response = conn.get("/congress/v1/members/house/CO/current.json")
+    # response = conn.get("/congress/v1/members/house/CO/current.json")
 
-    json = JSON.parse(response.body, symbolize_names: true)
-    @members = json[:results].map do |member_data|
-      Member.new(member_data)
-    end
+    # json = JSON.parse(response.body, symbolize_names: true)
+    # @members = json[:results].map do |member_data|
+    #   Member.new(member_data)
+    # end
   end
 end
---
+++ app/facades/search_facade.rb
@@ -0,0 +1,17 @@
+class SearchFacade
+  def self.members(state)
+    # state = params[:state]
+
+    conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
+      faraday.headers["X-API-Key"] =  ENV["PROPUBLICA_API_KEY"]
+    end
+
+    response = conn.get("/congress/v1/members/house/CO/current.json")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    @members = json[:results].map do |member_data|
+      Member.new(member_data)
+    end
+  end
+  
+end
